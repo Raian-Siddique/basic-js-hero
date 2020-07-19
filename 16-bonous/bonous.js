e9e5094f9d5d30ac8fbd62fb3f00247af471a957
Raian-Siddique <raian0197@gmail.com>
2020-07-19 06:04:36 +0600

    folder arrange

--- 16-bonous/bonous.js
+++ 16-bonous/bonous.js
@@ -22,3 +22,16 @@
 
 // var result2 = add(144, 568);
 // console.log(result2);
+
+
+function factorial(num) {
+    var fact = 1;
+    for (var i = 1; i <= num; i++) {
+        fact = fact * i;
+        console.log(i, fact);
+    }
+    return fact;
+}
+var result = factorial(5);
+console.log('result', result);
+cha
