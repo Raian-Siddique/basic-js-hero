26d904f40d463d5045c474a3b4ada618d546d0d8
Raian-Siddique <raian0197@gmail.com>
2020-08-07 07:17:19 +0600

    entering into es-6 module

--- academind-js/source-code/basics-01-starting-project/assets/scripts/app.js
+++ academind-js/source-code/basics-01-starting-project/assets/scripts/app.js
@@ -1,4 +1,16 @@
-let currentResult = 0;
+let defaultResult = 0;
+let currentResult = defaultResult;
+
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('This result is ' + result);
+
+}
+add(1, 2);
+add(5, 6);
+
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
-let calculationDescription = `( ${currentResult} + 10 ) *3 / 2 - 1`;
+let calculationDescription = `( ${defaultResult} + 10 ) * 3 / 2 - 1`;
 outputResult(currentResult, calculationDescription);
---
+++ academind-js/source-code/basics-01-starting-project/joke.js
@@ -0,0 +1,7 @@
+var count = 0;
+count = newCount;
+
+function newCount() {
+    return count++;
+}
+console.log(newCount('raian')); // displays 0`
