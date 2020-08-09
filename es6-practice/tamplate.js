1e4cd5c5969296bf82ecd9b8a024c03f713d8761
Raian-Siddique <raian0197@gmail.com>
2020-08-09 11:19:52 +0600

    the threedots

---
+++ es6-practice/tamplate.js
@@ -0,0 +1,10 @@
+const firstName = "Justine";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + "is a good boy.";
+const fullName2 = `${firstName} ${60+40} is a good boy.`;
+// console.log(fullName2);
+const multiline2 = `I love you 
+i muss you 
+i dont need you 
+go die with your new boyfriend.`
+console.log(multiline2);
