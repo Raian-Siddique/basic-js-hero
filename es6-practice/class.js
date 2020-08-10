7748af2f7ed41b9ed3d0e533e95e0cba00417b97
Raian-Siddique <raian0197@gmail.com>
2020-08-10 16:53:39 +0600

    inheritnace done

---
+++ es6-practice/class.js
@@ -0,0 +1,14 @@
+class Student {
+    constructor(sId, sName) {
+        this.id = sId;
+        this.name = sName;
+        this.school = "kolimunnesa";
+
+    }
+}
+const student1 = new Student(12, "shuvo");
+const student2 = new Student(22, "mahiya");
+const student3 = new Student(29, "Bappi");
+// console.log(student1.name, student2.name);
+// console.log(student1, student2);
+console.log(student3);
