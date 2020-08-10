bc9019553d75e1fcffffe6f7e3bd3097db99d3d8
Raian-Siddique <raian0197@gmail.com>
2020-08-10 19:24:16 +0600

    this is my True-False battle

---
+++ advanced-javascript/true-false.js
@@ -0,0 +1,19 @@
+// Falsy- false, 0 , "", undefined, null, NaN,
+//  Truthy- '0', ' ', [];
+
+
+
+// const age = 4;
+// if (age > 3) {
+//     console.log("condition is true ");
+
+// } else {
+//     console.log("condition is false");
+// }
+const name = 12;
+if (name || name == 0) {
+    console.log("condition is true ");
+
+} else {
+    console.log("condition is false");
+}
