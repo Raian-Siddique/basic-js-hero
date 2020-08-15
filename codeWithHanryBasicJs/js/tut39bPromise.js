e1a0871de323f780b127f80f891bfcabf148d0ad
Raian-Siddique <raian0197@gmail.com>
2020-08-15 22:46:11 +0600

    the lasr editin of promis is done

--- codeWithHanryBasicJs/js/tut39bPromise.js
+++ codeWithHanryBasicJs/js/tut39bPromise.js
@@ -1,25 +1,66 @@
-// // Promise: Best video on promises
-// // -resolve
-// // -reject
-// // -pending
-
-function func1() {
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-            const error = true;
-            if (!error) {
-                console.log('Function: Your promise has been resolved')
-                resolve();
-            } else {
-                console.log('Function: Your promise has not been resolved')
-                reject('Sorry not fulfilled');
-            }
-        }, 2000);
-    })
+const students = [
+  {
+    name: "Harry",
+    subject: "Javascript",
+  },
+  {
+    name: "Rohan",
+    subject: "Machine Learning",
+  },
+];
+function enrollStudent(student) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      students.push(student);
+      console.log("Student has been enrolled");
+      const error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 1000);
+  });
 }
 
-func1().then(function () {
-    console.log("Harry: Thanks for resolving")
-}).catch(function (error) {
-    console.log("Harry: Very bad bro. Reason: " + error)
-})
+function getStudents() {
+  setTimeout(() => {
+    let str = "";
+    students.forEach(function (student) {
+      str += `<li> ${student.name} </li>`;
+    });
+    document.getElementById("students").innerHTML = str;
+    console.log("Student have been fetched");
+  }, 3000);
+}
+
+let newStudent = {
+  name: "sunny",
+  subject: "Python",
+};
+enrollStudent(newStudent)
+  .then(getStudents)
+  .catch(() => {
+    console.log("some error ocured");
+  });
+
+// function func1() {
+//     return new Promise(function (resolve, reject) {
+//         setTimeout(() => {
+//             const error = true;
+//             if (!error) {
+//                 console.log('Function : You function has been resolved');
+//                 resolve();
+
+//             } else {
+//                 console.log('Function: your function has not been resolved ');
+//                 reject(' Sorry not fulfilled !');
+//             }
+//         }, 2000);
+//     })
+// }
+// func1().then(function () {
+//     console.log("harry: Thanks for your resolving");
+// }).catch(function (error) {
+//     console.log("Harry: Very bad bro. Reason:" + error);
+// })
